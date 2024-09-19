8dfc6234469a9b1b34873efad845b30c144336df
lykaalperes <lykaalperes@gmail.com>
2024-09-19 15:41:06 +0800

    #1 displayhelloworld

---
+++ displayhelloworld/displayhelloworld.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-displayhelloworld',
+  templateUrl: './displayhelloworld.component.html',
+  styleUrls: ['./displayhelloworld.component.css']
+})
+export class DisplayhelloworldComponent {
+  helloMessage: string = 'Hello World';
+}
