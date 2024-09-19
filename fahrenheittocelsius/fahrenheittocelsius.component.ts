d393414fdc7d56c66f26af54c7e3f60b25684b07
lykaalperes <lykaalperes@gmail.com>
2024-09-19 15:56:38 +0800

    #17 fahrenheittocelsius

---
+++ fahrenheittocelsius/fahrenheittocelsius.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fahrenheittocelsius',
+  templateUrl: './fahrenheittocelsius.component.html',
+  styleUrls: ['./fahrenheittocelsius.component.css']
+})
+export class FahrenheittocelsiusComponent {
+  fahrenheit: number | null = null;
+  celsius: number | null = null;
+
+  convertToCelsius() {
+    if (this.fahrenheit !== null) {
+      this.celsius = (this.fahrenheit - 32) * 5 / 9;
+    }
+  }
+}
