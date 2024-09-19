ea5021cf03db6fb560c663c81fefc4dd41737ec5
lykaalperes <lykaalperes@gmail.com>
2024-09-19 15:58:42 +0800

    #36 oddsumcalculator

---
+++ oddsumcalculator/oddsumcalculator.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oddsumcalculator',
+  templateUrl: './oddsumcalculator.component.html',
+  styleUrls: ['./oddsumcalculator.component.css']
+})
+export class OddsumcalculatorComponent {
+  number: number | null = null;
+  oddSum: number | null = null;
+
+  calculateOddSum() {
+    if (this.number !== null && this.number >= 0) {
+      this.oddSum = 0;
+      for (let i = 1; i <= this.number; i += 2) {
+        this.oddSum += i;
+      }
+    } else {
+      this.oddSum = null;
+    }
+  }
+}
